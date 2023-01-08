c38136b7a2e4ed8a518ab86243651547d28a72fc
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-01-08 13:07:08 -0500

    KEC - ApiGateway added

--- kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/LabsGreetingGETName.java
+++ kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/LabsGreetingGETName.java
@@ -5,7 +5,7 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 
 import java.io.*;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 public class LabsGreetingGETName implements RequestStreamHandler {
@@ -14,25 +14,18 @@ public class LabsGreetingGETName implements RequestStreamHandler {
     /**
      * This method is called when the Lambda function is invoked.
      * Function to get the name of the person.
-     * @param inputStream
-     * @param outputStream
-     * @param context
-     * @throws IOException
      */
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         LambdaLogger logger = context.getLogger();
-        try {
+        try (inputStream; outputStream) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,
-                    Charset.forName("UTF-8")));
+                    StandardCharsets.UTF_8));
             PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream,
-                    Charset.forName("UTF-8"))));
+                    StandardCharsets.UTF_8)));
             writer.write("{\"name\":\"John\"}");
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.log("Error in getting the name");
-        }finally {
-            inputStream.close();
-            outputStream.close();
         }
 
     }
