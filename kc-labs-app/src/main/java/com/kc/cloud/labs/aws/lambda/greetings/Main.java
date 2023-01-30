a2f1981fe622715e2a32f8c7f59de19bef41a091
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-01-30 18:30:40 -0500

    KEC - Add libs test

--- kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/Main.java
+++ kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/Main.java
@@ -1,13 +1,16 @@
 package com.kc.cloud.labs.aws.lambda.greetings;
 
+import com.kec.libs.utils.LibCore;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        String code = RandomStringUtils.random(10, true, true);
-        System.out.println(code);
+        List<String> random = List.of("x", "y","z");
+        String json = LibCore.listToJson(random);
+        System.out.println(json);
     }
 }
