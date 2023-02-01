b6bd67b8515cb24a570e65e8ab30855fd306553d
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-02-01 17:32:51 -0500

    Revert "KEC - Add libs test"
    This reverts commit a2f1981fe622715e2a32f8c7f59de19bef41a091.

--- kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/Main.java
+++ kc-labs-app/src/main/java/com/kc/cloud/labs/aws/lambda/greetings/Main.java
@@ -1,16 +1,13 @@
 package com.kc.cloud.labs.aws.lambda.greetings;
 
-import com.kec.libs.utils.LibCore;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.*;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> random = List.of("x", "y","z");
-        String json = LibCore.listToJson(random);
-        System.out.println(json);
+        String code = RandomStringUtils.random(10, true, true);
+        System.out.println(code);
     }
 }
