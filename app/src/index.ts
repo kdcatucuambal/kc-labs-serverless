894861976723a5394a1c731c1695de3953938a39
Kevin Catucuamba <65633861+kdcatucuambal@users.noreply.github.com>
2023-04-06 21:23:50 -0500

    KEC - API Gateway Cors is configure (initial)

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,7 +2,7 @@ import { httpClient } from "./util/httpClient";
 
 const btn1 = document.getElementById('btn1') as HTMLButtonElement;
 btn1.addEventListener('click', async () => {
-    const response = await httpClient.get('balances/1050');
+    const response = await httpClient.get('balances');
     console.log(response);
     console.log(httpClient)
 });
--- app/src/util/httpClient.ts
+++ app/src/util/httpClient.ts
@@ -4,6 +4,8 @@ export const httpClient = Axios.create({
     baseURL: "https://zhj834bu8l.execute-api.us-east-1.amazonaws.com/v1/labs",
     headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        "Accept": "application/json",
+        "x-request-id": "test",
+        // "Authorization": "Bearer 123"
     }
 });
