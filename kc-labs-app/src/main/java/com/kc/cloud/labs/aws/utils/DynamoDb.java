5a427fc8254dc64f7f2f1407eac19d1058a254a9
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-07-08 18:42:36 -0500

    Update DynamoDb.java

--- kc-labs-app/src/main/java/com/kc/cloud/labs/aws/utils/DynamoDb.java
+++ kc-labs-app/src/main/java/com/kc/cloud/labs/aws/utils/DynamoDb.java
@@ -1,13 +1,18 @@
 package com.kc.cloud.labs.aws.utils;
 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class DynamoDb {
 
     public static DynamoDbClient getClient(){
-        return DynamoDbClient.builder().region(Region.US_EAST_1).build();
+        SdkHttpClient httpClient = UrlConnectionHttpClient.builder().build();
+        return DynamoDbClient.builder()
+                .httpClient(httpClient)
+                .region(Region.US_EAST_1).build();
     }
 
     public static DynamoDbEnhancedClient getEnhancedClient(){
