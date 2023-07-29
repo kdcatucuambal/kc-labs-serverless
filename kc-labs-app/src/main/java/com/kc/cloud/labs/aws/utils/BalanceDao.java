52ccda1a11df98ab287a02ffa8950064fc4b2fcb
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2023-07-29 17:17:24 -0500

    KEC - Imlement dao from lib

--- kc-labs-app/src/main/java/com/kc/cloud/labs/aws/utils/BalanceDao.java
+++ kc-labs-app/src/main/java/com/kc/cloud/labs/aws/utils/BalanceDao.java
@@ -1,11 +1,12 @@
 package com.kc.cloud.labs.aws.utils;
 
+import com.kc.cloud.dao.BaseDao;
 import com.kc.cloud.labs.aws.models.app.Balance;
 
 public class BalanceDao extends BaseDao<Balance> {
 
     public BalanceDao(){
-        super("kec_balances", "bal", Balance.class);
+        super("labs_balances", "bal", Balance.class);
     }
 
 }
